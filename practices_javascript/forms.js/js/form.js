c22d1cba87166b92fc7ceb4c7cd46ff7cdca3307
mariomegu-learn <mariomegu.learn@gmail.com>
2022-06-10 21:55:08 -0500

    Practice JavaScript

---
+++ practices_javascript/forms.js/js/form.js
@@ -0,0 +1,63 @@
+(function(){
+    var formulario = document.getElementById('formulario'),
+        nombre = formulario.nombre,
+        correo = formulario.correo,
+        sexo = formulario.sexo,
+        terminos = formulario.terminos,
+        error = document.getElementById('error');
+
+    function validarNombre(e){
+        if(nombre.value == '' || nombre.value == null){
+            console.log('Por favor completa el campo nombre');
+            error.style.display = 'block';
+            error.innerHTML = error.innerHTML + '<li>Por favor completa el campo nombre</li>'
+            e.preventDefault();
+        } else {
+            error.style.display = 'none'
+        }
+    }
+
+    function validarCorreo(e){
+        if(correo.value == '' || correo.value == null){
+            console.log('Por favor completa el campo correo');
+            error.style.display = 'block';
+            error.innerHTML += '<li>Por favor completa el campo correo</li>'
+            e.preventDefault();
+        } else {
+            error.style.display = 'none'
+        }
+    }
+
+    function validarSexo(e){
+        if(sexo.value == '' || sexo.value == null){
+            console.log('Por favor completa el campo sexo');
+            error.style.display = 'block';
+            error.innerHTML += '<li>Por favor completa el campo Sexo</li>'
+            e.preventDefault();
+        } else {
+            error.style.display = 'none'
+        }
+    }
+
+    function validarTerminos(e){
+        if (terminos.checked == false){
+            console.log('Para continuar acepta los terminos');
+            error.style.display = 'block';
+            error.innerHTML += '<li>Para continuar acepta los terminos</li>'
+            e.preventDefault();
+        }
+        else {
+            error.style.display = 'none'
+        }
+    }
+
+    function validarFormulario(e){
+        error.innerHTML = '';
+        validarNombre(e);
+        validarCorreo(e);
+        validarSexo(e);
+        validarTerminos(e);
+    }
+    
+    formulario.addEventListener('submit', validarFormulario)
+}())
